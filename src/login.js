aaf33f10bd821e496c1c35a945522a3d7b59787f
Pothuri.Varma <Pothuri.Varma@INBNG01LT521.as.aecomnet.com>
2022-05-09 16:18:17 +0530

    Login background color changed from black to red

---
+++ src/login.js
@@ -0,0 +1,13 @@
+
+        function CheckUser(){
+             var UserName = document.getElementById("UserName").value;
+             alert(UserName);
+            if(UserName =="Varma") {
+              
+                window.open("pwd.html");
+            }
+           else {
+                 alert("invaild UserName") 
+            }
+        }  
+
