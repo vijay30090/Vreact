0ee21132fd8e036abffcede89ec7e7bc926309df
Pothuri.Varma <Pothuri.Varma@INBNG01LT521.as.aecomnet.com>
2022-05-09 15:41:39 +0530

    My Table color and width changed

--- src/Components/CustomTable.js
+++ src/Components/CustomTable.js
@@ -27,7 +27,7 @@ function CustomTable() {
             <h1>My Table</h1>
 
         </header>
-        <section style={{margin:"0 auto", width:"50%",backgroundColor:"brown"}}>
+        <section style={{margin:"0 auto", width:"100%",backgroundColor:"brown"}}>
             <table border="1">
                 <tr>
                     <th>Name</th>
